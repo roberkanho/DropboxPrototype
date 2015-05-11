95648ae72f9024dc195c48eaadfd20e6d92067da
roberkanho <nanchen@Nans-MacBook-Pro.local>
2015-05-10 23:29:55 -0700

    optional task completed

---
+++ DetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.swift
+//  Dropbox Prototype
+//
+//  Created by Nan Chen on 5/9/15.
+//  Copyright (c) 2015 Nan Chen. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var favButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+        
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func favButtonPressed(sender: AnyObject) {
+        
+        favButton.selected = !favButton.selected
+        
+            let defaults = NSUserDefaults.standardUserDefaults()
+            defaults.setBool(favButton.selected, forKey: "FavOnOff")
+            defaults.synchronize()
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SignInViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SignInViewController.swift
+//  Dropbox Prototype
+//
+//  Created by Nan Chen on 5/10/15.
+//  Copyright (c) 2015 Nan Chen. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailAddressField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        passwordField.secureTextEntry = true
+        emailAddressField.becomeFirstResponder()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
